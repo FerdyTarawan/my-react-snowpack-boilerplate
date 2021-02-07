a7662e1791c53979de7dd2949f30f8c9880ecede
FerdyTarawan <ferdy.tarawan@gmail.com>
2021-02-08 06:08:39 +0700

    adjust imported react-use hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,4 +4,3 @@ export * from '@app/hooks/reactHookForm';
 export * from '@app/hooks/useTranslation';
 export * from '@app/hooks/useStore';
 export * from '@app/hooks/useImmer';
-export * from '@app/hooks/useInterval';
