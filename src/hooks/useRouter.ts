d34bb11f51353f36e710a908f1dff6c6b4c36341
FerdyTarawan <ferdy.tarawan@gmail.com>
2021-02-13 17:56:21 +0700

    fix useRouter hook

--- src/hooks/useRouter.ts
+++ src/hooks/useRouter.ts
@@ -1,19 +1,19 @@
-import { useLocation, useMatch, useNavigate, useParams } from '@reach/router';
+import { useLocation, useNavigate, useParams } from '@reach/router';
 import { useMemo } from 'react';
 
-export const useRouter = (pathname?: string) => {
+export { useMatch } from '@reach/router';
+
+export const useRouter = () => {
   const params = useParams();
   const location = useLocation();
   const navigate = useNavigate();
-  const match = useMatch(pathname ?? '');
 
   return useMemo(() => {
     return {
       location,
-      match,
       navigate,
       params,
       pathname: location.pathname,
     };
-  }, [params, match, location, navigate]);
+  }, [params, location, navigate]);
 };
